8c6119e5b1bf00750196d6c8d32a16c83da5377a
田博 <tianbo@qingting.fm>
2019-12-09 17:48:46 +0800

    修改git clone仓库

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ program.version('1.0.0', '-v, --version')
       ]).then((answers) => {
         const spinner = ora('正在下载模板...');
         // spinner.start();
-        download('MGLHF/react_demo_js#master', name, { clone: true }, (err) => {
+        download('MGLHF/tb_cli_pro#master', name, { clone: true }, (err) => {
           if (err) {
             spinner.fail();
             console.log(symbols.error, chalk.red(err));
