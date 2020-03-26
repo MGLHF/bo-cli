ac0a9a88c0138e99f3f7eda91929df1e88602c36
田博 <tianbo@qingting.fm>
2020-03-26 11:44:38 +0800

    微前端项目自动搭建

--- index.js
+++ index.js
@@ -22,13 +22,13 @@ program.version('1.0.0', '-v, --version')
         }
       ]).then((answers) => {
         const spinner = ora('正在下载模板...');
-        // spinner.start();
-        download('MGLHF/tb_cli_pro#master', name, { clone: true }, (err) => {
+        spinner.start();
+        download('MGLHF/tb-cli-pro#master', name, { clone: true }, (err) => {
           if (err) {
-            spinner.fail();
+            spinner.stop();
             console.log(symbols.error, chalk.red(err));
+            return;
           } else {
-            spinner.succeed();
             const fileName = `${name}/package.json`;
             const meta = {
               name,
@@ -40,7 +40,13 @@ program.version('1.0.0', '-v, --version')
               const result = handlebars.compile(content)(meta);
               fs.writeFileSync(fileName, result);
             }
-            console.log(symbols.success, chalk.green('项目初始化完成'));
+            spinner.stop();
+            console.log(`${symbols.success} ${chalk.green('项目初始化完成')}`);
+            console.log(`${chalk.bgWhite.black('项目启动')}`);
+            console.log(`${chalk.yellow(`cd ${name}`)}`);
+            console.log(`${chalk.yellow('npm install')}`);
+            console.log(`${chalk.yellow('npm run dev')}`);
+            console.log(`${chalk.green(`访问地址：localhost:8082`)}`);
           }
         })
       })
